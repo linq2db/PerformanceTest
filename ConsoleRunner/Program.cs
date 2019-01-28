5fb762818ced5366009de416cdcbee84119b6a26
Igor Tkachev <igor.tkachev@gmail.com>
2019-01-27 21:10:32 -0500

    Replaces Code.2.1 & NET.4.72 projects with ConsoleRunner.

---
+++ ConsoleRunner/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleRunner
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+#if NET472
+			Tests.LinqTestRunner.Run("NET 4.72", true);
+#elif NETCOREAPP2_1
+			Tests.LinqTestRunner.Run("Core 2.1", false);
+#else
+#error Unknown platform.
+#endif
+		}
+	}
+}
