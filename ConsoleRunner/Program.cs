c826921f9487faa4ab9be1dc7c8ff30952f448b0
Igor Tkachev <igor.tkachev@gmail.com>
2024-02-01 21:38:03 -0500

    Switch to .net 8.

--- ConsoleRunner/Program.cs
+++ ConsoleRunner/Program.cs
@@ -10,8 +10,8 @@ namespace ConsoleRunner
 			Tests.LinqTestRunner.Run("NET 4.8", true, args);
 #elif NET6_0
 			Tests.LinqTestRunner.Run("NET 6.0", false, args);
-#elif NET7_0
-			Tests.LinqTestRunner.Run("NET 7.0", false, args);
+#elif NET8_0
+			Tests.LinqTestRunner.Run("NET 8.0", false, args);
 #else
 #error Unknown platform.
 #endif
